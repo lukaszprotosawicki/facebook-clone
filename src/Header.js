91026250fa00d33ae525f10af53245665f0caf91
lukasz protosawicki <lukas.es.p@o2.pl>
2022-02-15 21:33:17 +0100

    add materials ui + icons + header

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1024px-Facebook_f_logo_%282019%29.svg.png"
+          alt=""
+        />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
