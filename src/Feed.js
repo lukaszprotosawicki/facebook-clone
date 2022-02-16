084587af9caaf006f0cc75d1d681ecbfdb511b03
lukasz protosawicki <lukas.es.p@o2.pl>
2022-02-16 21:22:32 +0100

    add post.js + post.css

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 import StoryReel from "./StoryReel";
 
 function Feed() {
@@ -8,6 +9,15 @@ function Feed() {
     <div className="feed">
       <StoryReel />
       <MessageSender />
+      <Post
+        profilePic="https://images.unsplash.com/photo-1544465544-1b71aee9dfa3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBob3RvfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+        message="działa"
+        timestamp="this is a timestamp"
+        username="lucaretttttti"
+        image="https://images.unsplash.com/photo-1610962895729-2975a9717fad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29vbCUyMHBob3RvfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+      />
+      <Post />
+      <Post />
     </div>
   );
 }
